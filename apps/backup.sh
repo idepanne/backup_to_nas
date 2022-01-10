fd433c508e4658db5647285313c92c4ff5f0d33c
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-01-10 14:47:11 +0100

    Add files via upload

--- apps/backup.sh
+++ apps/backup.sh
@@ -2,7 +2,7 @@
 cd
 echo "###############################################################################"
 echo "#                                                                             #"
-echo "#                             Backup to NAS 2.2.0                             #"
+echo "#                             Backup to NAS 2.1.3                             #"
 echo "#                 © 2021-2022 iDépanne – L'expert informatique                 #"
 echo "#                           https://fb.me/idepanne/                           #"
 echo "#                            idepanne67@gmail.com                             #"
