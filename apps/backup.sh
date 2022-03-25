ff908ebf8b4875e4274b9820de0db4c9185cb7ed
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-03-25 23:31:35 +0100

    Add files via upload

--- apps/backup.sh
+++ apps/backup.sh
@@ -4,7 +4,7 @@ cd
 echo "+=============================================================================+"
 echo "|                                Backup to NAS                                |"
 echo "|                                  backup.sh                                  |"
-echo "|                                    [224]                                    |"
+echo "|                                    [225]                                    |"
 echo "|                © 2020-2022 iDépanne – L'expert informatique                 |"
 echo "|                            idepanne67@gmail.com                             |"
 echo "+=============================================================================+"
@@ -26,7 +26,7 @@ echo ""
 echo "Source       :  /home/`echo "$var2"`/"
 echo "Destination  :  NAS_PATH:/`echo "$var1"`/`echo "$var2"`/"
 echo ""
-rclone sync -v -L -P --ignore-errors --exclude=snap/** --exclude=.dbus/** --exclude='VirtualBox VMs'/** /home/`echo "$var2"`/ NAS_PATH:/`echo "$var1"`/`echo "$var2"`/
+rclone sync -v -L -P --ignore-errors --exclude=snap/** --exclude=.dbus/** --exclude=.config/pulse --exclude=.anydesk --exclude='VirtualBox VMs'/** /home/`echo "$var2"`/ NAS_PATH:/`echo "$var1"`/`echo "$var2"`/
 echo ""
 echo "+=============================================================================+"
 echo "|                        **** Sauvegarde terminée ****                        |"
