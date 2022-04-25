41059af8733c68aeafe294194cc62fa0d7f8d4a9
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-04-25 11:41:49 +0200

    Add files via upload

--- apps/backup.sh
+++ apps/backup.sh
@@ -4,30 +4,54 @@ cd
 echo "+=============================================================================+"
 echo "|                                Backup to NAS                                |"
 echo "|                                  backup.sh                                  |"
-echo "|                                    [26]                                     |"
+echo "|                                    [31]                                     |"
 echo "|                © 2020-2022 iDépanne – L'expert informatique                 |"
 echo "|                            idepanne67@gmail.com                             |"
 echo "+=============================================================================+"
 echo ""
 echo ""
+echo ""
+varsys=$(cat /etc/os-release | grep PRETTY_NAME | cut -c14- | rev | cut -c2- | rev)
+echo -n "Système      : "; echo "$varsys"
+echo ""
+echo ""
+echo ""
+if [[ $varsys == *"Manjaro"* ]]; then
+	varman=$(hostnamectl --static)
+	varusr=$(whoami)
+	echo "+=============================================================================+"
+	echo "|                    **** Démarrage de la sauvegarde ****                     |"
+	echo "+=============================================================================+"
+	echo ""
+	echo -n "Ordinateur   :  "; echo "$varman"
+	echo -n "Utilisateur  :  "; echo "$varusr"
+	echo ""
+	echo "Source       :  /home/`echo "$varusr"`/"
+	echo "Destination  :  NAS_PATH:/`echo "$varman"`/`echo "$varusr"`/"
+	echo ""
+	rclone sync -v -L -P --ignore-errors --exclude=snap/** --exclude=.dbus/** --exclude=.config/pulse --exclude=.anydesk --exclude='VirtualBox VMs'/** /home/`echo "$varusr"`/ NAS_PATH:/`echo "$varman"`/`echo "$varusr"`/
+	echo ""
+	echo "+=============================================================================+"
+	echo "|                        **** Sauvegarde terminée ****                        |"
+	echo "+=============================================================================+"
 
-###### Définition des variables ######
-var1=$(hostname)
-var2=$(whoami)
-######################################
+else
 
-echo "+=============================================================================+"
-echo "|                    **** Démarrage de la sauvegarde ****                     |"
-echo "+=============================================================================+"
-echo ""
-echo -n "Ordinateur   :  "; echo "$var1"
-echo -n "Utilisateur  :  "; echo "$var2"
-echo ""
-echo "Source       :  /home/`echo "$var2"`/"
-echo "Destination  :  NAS_PATH:/`echo "$var1"`/`echo "$var2"`/"
-echo ""
-rclone sync -v -L -P --ignore-errors --exclude=snap/** --exclude=.dbus/** --exclude=.config/pulse --exclude=.anydesk --exclude='VirtualBox VMs'/** /home/`echo "$var2"`/ NAS_PATH:/`echo "$var1"`/`echo "$var2"`/
-echo ""
-echo "+=============================================================================+"
-echo "|                        **** Sauvegarde terminée ****                        |"
-echo "+=============================================================================+"
+	vardeb=$(hostname)
+	varusr=$(whoami)
+	echo "+=============================================================================+"
+	echo "|                    **** Démarrage de la sauvegarde ****                     |"
+	echo "+=============================================================================+"
+	echo ""
+	echo -n "Ordinateur   :  "; echo "$vardeb"
+	echo -n "Utilisateur  :  "; echo "$varusr"
+	echo ""
+	echo "Source       :  /home/`echo "$varusr"`/"
+	echo "Destination  :  NAS_PATH:/`echo "$vardeb"`/`echo "$varusr"`/"
+	echo ""
+	rclone sync -v -L -P --ignore-errors --exclude=snap/** --exclude=.dbus/** --exclude=.config/pulse --exclude=.anydesk --exclude='VirtualBox VMs'/** /home/`echo "$varusr"`/ NAS_PATH:/`echo "$vardeb"`/`echo "$varusr"`/
+	echo ""
+	echo "+=============================================================================+"
+	echo "|                        **** Sauvegarde terminée ****                        |"
+	echo "+=============================================================================+"
+fi
