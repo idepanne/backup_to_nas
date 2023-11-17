cedca8401c7c57f4b5c29d52f5b76de672a1f6f9
iDépanne <69732672+idepanne@users.noreply.github.com>
2023-11-17 01:02:33 +0100

    Add files via upload

--- apps/backup.sh
+++ apps/backup.sh
@@ -4,7 +4,7 @@ cd || return
 echo "+=============================================================================+"
 echo "|                                Backup to NAS                                |"
 echo "|                                  backup.sh                                  |"
-echo "|                                    [127]                                    |"
+echo "|                                    [128]                                    |"
 echo "|                © 2020-2023 iDépanne – L'expert informatique                 |"
 echo "|                        idepanne.support.tech@free.fr                        |"
 echo "+=============================================================================+"
@@ -13,11 +13,11 @@ echo ""
 
 ########## Définition des variables ##########
 varsys=$(< /etc/os-release grep PRETTY_NAME)
-if [[ $varsys == *"EndeavourOS"* ]]; then
-	varsys=$(< /etc/os-release grep PRETTY_NAME | cut -c13-)
-else
+#if [[ $varsys == *"EndeavourOS"* ]]; then
+#	varsys=$(< /etc/os-release grep PRETTY_NAME | cut -c13-)
+#else
 	varsys=$(< /etc/os-release grep PRETTY_NAME | cut -c14- | rev | cut -c2- | rev)
-fi
+#fi
 
 vararchi1=$(uname -m)
 if [[ $vararchi1 == *"aarch"* ]]; then
@@ -34,12 +34,12 @@ else
 	fi
 fi
 
-varitfc1=$(ls /usr/bin/*session)
-if [[ $varitfc1 == *"lxsession"* || $varitfc1 == *"openbox"* || $varitfc1 == *"pipewire-media"* || $varitfc1 == *"xfce"* || $varitfc1 == *"gnome"* || $varitfc1 == *"kde"* || $varitfc1 == *"cinnamon"* || $varitfc1 == *"mate"* ]]; then
-	varitfc2="Graphique"
-else
-	varitfc2="Lignes de commandes"
-fi
+#varitfc1=$(ls /usr/bin/*session)
+#if [[ $varitfc1 == *"lxsession"* || $varitfc1 == *"openbox"* || $varitfc1 == *"pipewire-media"* || $varitfc1 == *"xfce"* || $varitfc1 == *"gnome"* || $varitfc1 == *"kde"* || $varitfc1 == *"cinnamon"* || $varitfc1 == *"mate"* ]]; then
+#	varitfc2="Graphique"
+#else
+#	varitfc2="Lignes de commandes"
+#fi
 
 ip=$(< ~/.config/rclone/rclone.conf grep host | cut -c8-)
 dest=$(< ~/.config/rclone/rclone.conf grep remote | cut -c20-)
@@ -47,7 +47,7 @@ dest=$(< ~/.config/rclone/rclone.conf grep remote | cut -c20-)
 
 echo -n "Système      :  "; echo "$varsys"
 echo -n "Processeur   :  "; echo "$vararchi2"
-echo -n "Interface    :  "; echo "$varitfc2"
+#echo -n "Interface    :  "; echo "$varitfc2"
 echo ""
 varcmp=$(uname -n)
 varusr=$(whoami)
@@ -90,7 +90,7 @@ if ping -c 3 $ip; then
     echo "+-----------------------------------------------------------------------------+"
     echo ""
     echo ""
-    echo "Déconnexion du serveur NAS"
+    echo "[V] Déconnexion"
 else
     echo ""
 	echo "[X] Serveur NAS introuvable !"
